3ce1bf525ce430b20a4355d4ef75ac60c36ac91c
Pedro Silva <pedro_rike@hotmail.com.br>
2025-08-23 01:32:03 -0300

    feat: implement jwt auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,19 @@
 // src/auth/auth.controller.ts
-import { Controller, Post, Body } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { RegisterDriverDto } from './dto/register-driver.dto';
-import { SignInDriverDto } from './dto/signin-driver.dto';
+import { Controller, Post, Body } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { RegisterDriverDto } from "./dto/register-driver.dto";
+import { SignInDriverDto } from "./dto/signin-driver.dto";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
-  @Post('signup')
+  @Post("signup")
   async signup(@Body() dto: RegisterDriverDto) {
     return this.authService.signup(dto);
   }
 
-  @Post('login')
+  @Post("login")
   async signin(@Body() dto: SignInDriverDto) {
     return this.authService.signin(dto);
   }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,20 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
-import { JwtModule, JwtService } from "@nestjs/jwt";
+import { JwtModule } from "@nestjs/jwt";
 import { PrismaService } from "src/prisma/prisma.service";
 import { PrismaDriversRepository } from "src/repositories/prisma/prisma-driver-repository";
 import { SignInDriverUseCase } from "./use-cases/signin-driver-use-case";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import type { DriversRepository } from "src/repositories/drivers-repository";
+import { JwtStrategy } from "./guard/jwt.strategy";
 
 @Module({
   imports: [
-    ConfigModule,
+    ConfigModule.forRoot(),
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        secret: config.get<string>("JWT_SECRET"),
+        secret: config.get<string>("JWT_SECRET") || "default_secret",
         signOptions: { expiresIn: "1h" },
       }),
     }),
@@ -24,7 +24,7 @@ import type { DriversRepository } from "src/repositories/drivers-repository";
     AuthService,
     SignInDriverUseCase,
     PrismaService,
-    JwtService,
+    JwtStrategy,
     {
       provide: "DriversRepository",
       useClass: PrismaDriversRepository,
