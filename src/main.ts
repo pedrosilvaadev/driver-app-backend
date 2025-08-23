ac78555574b512c441ae9ba9bd2bab5cbb9a2582
Pedro Silva <pedro_rike@hotmail.com.br>
2025-08-23 10:54:34 -0300

    feat: add cors validation

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,5 +17,7 @@ async function bootstrap() {
       transform: true,
     })
   );
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
